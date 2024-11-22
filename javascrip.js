7aceb3c65ae39cf3350eed2f89bc1140b46ed089
Dielson Silva <133800091+odisilva@users.noreply.github.com>
2024-11-22 13:32:56 -0300

    Add files via upload

---
+++ javascrip.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetElement = document.querySelector(this.getAttribute('href'));
+        const headerOffset = 70; // Altura do cabeçalho fixo
+        const elementPosition = targetElement.getBoundingClientRect().top;
+        const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    });
+});
